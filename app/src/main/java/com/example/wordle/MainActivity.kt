b3d1d75aad563890fa09242c69a37b8649303b8a
georg469256 <michael.george20152@gmail.com>
2022-09-10 17:56:07 -0500

    Added button to submit guess

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -2,16 +2,18 @@ package com.example.wordle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.InputFilter
+import android.text.TextWatcher
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private val wordToGuess = FourLetterWordList.getRandomFourLetterWord()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val guess = findViewById<EditText>(R.id.userGuess)
-        guess.filters += InputFilter.AllCaps()
 
         /**
          * Parameters / Fields:
@@ -37,6 +39,30 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             return result
+        }
+
+        val guess = findViewById<EditText>(R.id.userGuess)
+
+        val guessButton = findViewById<Button>(R.id.guessButton)
+
+        guess.filters += InputFilter.AllCaps()
+
+        guess.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(input: CharSequence?, start: Int, before: Int, count: Int) {
+                guessButton.isEnabled = (count == 4)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        guessButton.setOnClickListener {
+            val guessResult = checkGuess(guess.text.toString())
+            Toast.makeText(this,"Word: $wordToGuess Guess: $guessResult", Toast.LENGTH_LONG).show()
         }
 
     }
