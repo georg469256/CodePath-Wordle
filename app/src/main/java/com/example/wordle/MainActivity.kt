49fcf1e50aca7aae8a64d7c407374174258088a9
georg469256 <michael.george20152@gmail.com>
2022-09-09 21:00:45 -0500

    Added EditText for user guess

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.wordle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val guess = findViewById<EditText>(R.id.userGuess)
+        guess.filters += InputFilter.AllCaps()
+
     }
+
 }
